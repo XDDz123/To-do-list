67dde50f6dd409703d80f1236c6b7a4a12e6d2f6
Zelong Li <zelongli@students.cs.ubc.ca>
2019-10-06 12:27:17 -0700

    Version 5.6.1 Added more tests

--- src/test/io/SaveAndLoadTest.java
+++ src/test/io/SaveAndLoadTest.java
@@ -1,6 +1,7 @@
 package io;
 
 import model.CompletedTask;
+import model.ImportantTask;
 import model.TaskList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.time.MonthDay;
+import java.time.Period;
+import java.time.Year;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -71,5 +74,42 @@ public class SaveAndLoadTest {
         + "/" + (MonthDay.now().getDayOfMonth() - 1));
         assertEquals(((CompletedTask) taskList.getTask(1)).getCompletionStatus(), "past due.");
         assertTrue(taskList.getTask(1) instanceof CompletedTask);
+    }
+
+    @Test
+    public void checkImportantTaskNextYear() {
+        ArrayList<String> partsOfLine = new ArrayList<>();
+        partsOfLine.add("*");
+        partsOfLine.add("unassigned");
+        partsOfLine.add("high");
+        partsOfLine.add(String.valueOf(MonthDay.now().getMonthValue() - 1));
+        partsOfLine.add(String.valueOf(MonthDay.now().getDayOfMonth() - 1));
+        partsOfLine.add("High Importance");
+        partsOfLine.add("2020");
+        saveAndLoad.setIncompleteTaskField(partsOfLine);
+        TaskList taskList = new TaskList();
+        saveAndLoad.checkImportantTaskLoad(partsOfLine, taskList);
+        Period difference = Period.between(
+                (taskList.getTask(1)).getDueDateObj().atYear(Year.now().getValue() + 1),
+                MonthDay.now().atYear(Year.now().getValue()));
+
+        assertEquals(((ImportantTask) taskList.getTask(1)).getTimeLeft(),
+                Math.abs(difference.getMonths()) + " months " + Math.abs(difference.getDays()) + " days.");
+    }
+
+    @Test
+    public void checkImportantTaskCurrentYear() {
+        ArrayList<String> partsOfLine = new ArrayList<>();
+        partsOfLine.add("*");
+        partsOfLine.add("unassigned");
+        partsOfLine.add("high");
+        partsOfLine.add(String.valueOf(MonthDay.now().getMonthValue() + 1));
+        partsOfLine.add(String.valueOf(MonthDay.now().getDayOfMonth() + 1));
+        partsOfLine.add("High Importance");
+        partsOfLine.add("2019");
+        saveAndLoad.setIncompleteTaskField(partsOfLine);
+        TaskList taskList = new TaskList();
+        saveAndLoad.checkImportantTaskLoad(partsOfLine, taskList);
+        assertEquals(((ImportantTask) taskList.getTask(1)).getTimeLeft(), "1 months 1 days.");
     }
 }
