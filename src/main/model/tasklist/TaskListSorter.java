7f0e6d8292eff6d4251f5177cf3793abbad751ed
Zelong Li <zelongli@students.cs.ubc.ca>
2019-11-25 19:21:16 -0800

    Version 11.2.1 Adjusted testing Added past due highlight Squashed bugs

--- src/main/model/tasklist/TaskListSorter.java
+++ src/main/model/tasklist/TaskListSorter.java
@@ -9,23 +9,6 @@ class TaskListSorter {
     //MODIFIES: taskList
     //EFFECTS: Sorts the current task list chronologically based on due dates. Starts from most recently due.
     void sortByDueDate(TaskList taskList) {
-/*        taskList.getTaskList().sort((a, b) -> {
-            if (a.isCompleted() && b.isCompleted()) {
-                return 0;
-            } else if (a.isCompleted()) {
-                return 1;
-            } else if (b.isCompleted()) {
-                return -1;
-            } else {
-                if (a.getDueDateObj().isBefore(b.getDueDateObj())) {
-                    return -1;
-                } else if (a.getDueDateObj().isAfter(b.getDueDateObj())) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        });*/
 
         taskList.getTaskList().sort((a, b) -> {
             if (!(a.isCompleted() && b.isCompleted())) {
